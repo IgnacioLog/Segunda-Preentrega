76cd4c07aa5cb406963e3a1642a8cc168b22c105
IgnacioLog <103467993+IgnacioLog@users.noreply.github.com>
2023-10-18 12:45:59 -0300

    Update product.js

--- dao/models/product.js
+++ dao/models/product.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
   name: String,
-  price: Number
+  price: Number,
+  category: String,
+  availability: Boolean
 });
 
 module.exports = mongoose.model('Product', productSchema);
